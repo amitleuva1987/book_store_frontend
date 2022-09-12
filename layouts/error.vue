daeee20cfa36a1829c238a0dc71c42b45323fe08
amitleuva1987 <amitleuva1987@gmail.com>
2022-09-12 17:38:42 +0530

    Created 404 error page

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="row" style="height: 500px">
+    <div class="col-md-12">
+      <h1 class="text-center p-5">404 Page not found</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
